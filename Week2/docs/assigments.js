e7ffa6b16ca5b7be8b64d4a970e1d1cafd95088e
Özcan Kara <ozcankara810@gmail.com>
2023-10-24 07:13:35 +0300

    some changes

--- Week2/docs/assigments.js
+++ Week2/docs/assigments.js
@@ -47,3 +47,51 @@ const pokemons = [
 //Pokemon dizisinde, her t�rdeki Pokemon'lar�n deneyim puanlar�n�n ortalamas�n� hesaplamak istiyorsunuz.
 // Her t�rdeki Pokemon'lar�n deneyim puanlar�n�n ortalamas�n� hesapla tiplerin karsiliginda ortalamasi 
 
+const typeExperienceAverages = {};
+pokemons.forEach(pokemon=>{
+   if (!typeExperienceAverages[pokemons.type]) {
+    typeExperienceAverages[pokemons.type]={
+        totalExperience:0,
+        count:0,
+    };
+   } 
+   typeExperienceAverages[pokemon.type].typeExperience += pokemon.experience;
+   typeExperienceAverages[pokemon.type].count += 1;
+})
+
+for (var type in typeExperienceAverages) {
+  typeExperienceAverages[type].averagesExperience = typeExperienceAverages[type].totalExperience.count
+}
+
+console.log(typeExperienceAverages);
+
+--------------------------------------------------------------------------------------------------------------
+const typeExperienceAverages2 = pokemons.reduce((result, pokemon) => {
+  if (!result[pokemon.type]) {
+    result[pokemon.type] = { totalExperience: 0, count: 0 };
+  }
+  result[pokemon.type].totalExperience += pokemon.experience;
+  result[pokemon.type].count += 1;
+  return result;
+}, {});
+
+for (const type in typeExperienceAverages) {
+  typeExperienceAverages[type].averageExperience =
+    typeExperienceAverages[type].totalExperience / typeExperienceAverages[type].count;
+}
+
+console.log(typeExperienceAverages2);
+
+const typeExperienceAverages3 = Object.values(
+  pokemons.reduce((result, pokemon) => {
+    result[pokemon.type] = result[pokemon.type] || { totalExperience: 0, count: 0 };
+    result[pokemon.type].totalExperience += pokemon.experience;
+    result[pokemon.type].count += 1;
+    return result;
+  }, {})
+).map(typeData => ({
+  type: typeData.totalExperience,
+  averageExperience: typeData.totalExperience / typeData.count
+}));
+
+console.log(typeExperienceAverages3);
