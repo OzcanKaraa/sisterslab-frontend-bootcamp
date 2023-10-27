9812c610a54350055e057cf7ebcb328db7d928dc
Özcan Kara <ozcankara810@gmail.com>
2023-10-27 05:28:35 +0300

    some changes

---
+++ Week3/applications/src/App.jsx
@@ -0,0 +1,23 @@
+import CoinFlipper from './components/CoinFlipper';
+import Counter from './components/Counter';
+import HamburgerApp from './components/HamburgerApp';
+import Text from './components/Text';
+import TodoApp from './components/TodoApp';
+
+const App = () => {
+  return (
+    <div>
+      {/* App
+      <br />
+      <Counter initialCount={0} minCount={0} maxCount={10} />
+      <br />
+      <Text placeholder="Type something" />
+      <br /> */}
+      {/* <CoinFlipper /> */}
+      {/* <HamburgerApp /> */}
+      <TodoApp />
+    </div>
+  );
+};
+
+export default App;
