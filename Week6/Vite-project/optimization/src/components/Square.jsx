5c9df282284e776debe44008f8cf86a38da66387
Özcan Kara <ozcankara810@gmail.com>
2023-11-14 04:08:13 +0300

    add: useMemo

---
+++ Week6/Vite-project/optimization/src/components/Square.jsx
@@ -0,0 +1,27 @@
+import { useMemo, useState } from "react"
+
+//usememo ile uslu sayılar hesaplama
+
+    const Square = () => {
+    const [number,setNumber] = useState(0);
+    const [inc,setInc] = useState(0);
+    const squareFunc = useMemo(()=>{
+        console.log('Kare Hesaplandi..')
+        return number*number
+    },[number]);
+
+
+  return (
+    <div>
+        <input type="number" value={number} 
+        onChange={e=>setNumber(parseInt(e.target.value , 10))}
+        />
+        <p>{number} sayisinin karesi: {squareFunc}</p>
+        <button onClick={() => setInc(inc+1)}>Yeniden Hesapla</button>
+        <p>Islemsiz Artis Deger: {inc}</p>
+    </div>
+  )
+}
+
+export default Square
+
