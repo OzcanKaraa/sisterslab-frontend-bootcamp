61fa06625a0613eb91536c4692f4dc1d70f6a349
Özcan Kara <ozcankara810@gmail.com>
2023-11-18 23:49:57 +0300

    some changes

--- Week6/Vite-project/optimization/src/components/OptimizedWithCb.jsx
+++ Week6/Vite-project/optimization/src/components/OptimizedWithCb.jsx
@@ -6,9 +6,12 @@ const OptimizedWithCb = () => {
         console.log('Artirma islemi gerceklesti :>> ')
         setCount((c)=>c+1)
    },[count]); //count degisip degistiginde kontrol edilir render olarak calissin.Optimizasyon
-   //},[setCount])  her defasinda render edilir.
+   // },[setCount])  her defasinda render edilir.
     
-    /*usecallback ile butona tıklandıgında fonksiyon calisir.Eger usecallback kullanilmasaydi counter companenti her render oldugunda increment fonksiyonu da yeniden olusturulacakti.Bu durum ozellikle increment fonksiyonu alt companente gecisi durumunda onemli olur.Fonksiyonun yeni referansi alt companentinde gereksiz yere yeniden render edilmesine yol aciyor.Bu companenti her gonderildignde fonksiyonun referansi da degisiyor.Referans degistigi icinde her seferinde tekrar render edilir.
+    /*usecallback ile butona tıklandıgında fonksiyon calisir.Eger usecallback kullanilmasaydi counter companenti her render oldugunda increment fonksiyonu da yeniden olusturulacakti.Bu durum ozellikle increment fonksiyonu alt companente gecisi durumunda onemli olur.
+    
+    Fonksiyonun yeni referansi alt companentinde gereksiz yere yeniden render edilmesine yol aciyor.Bu companenti her gonderildignde fonksiyonun referansi da degisiyor.Referans degistigi icinde her seferinde tekrar render edilir.
+
     useCallBack count degismedigi surece ayni increment fonksiyonu referansi korunmus olur.
     useCallBack ile referans kaydedilir tekrar render edilmesine gerek olmaz.Performansi iyilestirmis olunuyor.
     */
