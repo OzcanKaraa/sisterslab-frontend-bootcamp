506cf02e6a009c981020bff3cc5ee27d36603f6e
Özcan Kara <ozcankara810@gmail.com>
2023-11-23 20:29:40 +0300

    some changes

--- Week6/Nextjs-projects/book-shelf/src/pages/index.js
+++ Week6/Nextjs-projects/book-shelf/src/pages/index.js
@@ -15,18 +15,36 @@ const Home = () => {
   }, []);
 
   return;
-  /*
+
   <Grid container spacing={2}>
     {books.map((book) => (
       <Grid item key={book.id} xs={12} sm={6} md={4} lg={3}>
         <Card sx={{ maxWith: 345 }}>
-
+        <CardActionArea companent='img' height='140' image={book.imageUrl ?? ''}
         </Card>
       </Grid>
       //  <Link href= {`/books`}>
       //Grid ile sayfa size gore response olarak gosterme  xs={12} sm={6} md={4} lg={3} Grid 12 xs=1 sm 2 md 3 lg 4 tane kart gosterir.
     ))}
   </Grid>;
-  */
 };
 export default Home;
+
+
+{/* <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea> */}
